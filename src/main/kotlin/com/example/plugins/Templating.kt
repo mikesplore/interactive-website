e6854f63799ba9d3ad466fc775138c5a6d464644
mikesplore <mikepremium8@gmail.com>
2024-05-09 11:02:49 +0300

    website

--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
@@ -9,6 +10,8 @@ import io.ktor.server.routing.*
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+
     }
     routing {
     }
